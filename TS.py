6540a0e0abc5ff23d3fab5996c45ca0c48007cfb
TodorAtanassov <todoratanasov432@gmail.com>
2022-11-15 13:09:36 +0200

    Two sum

--- TS.py
+++ TS.py
@@ -1,9 +1,19 @@
-nums, target = input()
-
-
 class Solution:
-    def twoSum(self, nums, target):
+    def twosum(self, nums, target):
         for i in range(len(nums)):
             for j in range(len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+
+###
+
+
+class Solution:
+    def twosum(self, nums, target):
+        seen = {}
+        for i, num in enumerate(nums):
+            if target - num in seen:
+                return [seen[target - num], i]
+            else:
+                seen[num] = i
