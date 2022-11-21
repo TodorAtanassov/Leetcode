197f0f782ac396be3e7263aa51e28c1afead512c
TodorAtanassov <todoratanasov432@gmail.com>
2022-11-21 13:51:49 +0200

    fib

--- fib.py
+++ fib.py
@@ -1,8 +1,10 @@
 def fib(n):
-    if n ==0:
+    if n == 0:
         return 0
     elif n <= 2:
         return 1
-    else: return fib(n-1)+fib(n-2)
+    else:
+        return fib(n - 1) + fib(n - 2)
+
 
 print(fib(6))
